71d3b9a0fff1fb0591635e35f1e7340a7e90496b
Данил Алексеевич Зайцев <Giveknotgiveknot@gmail.com>
2023-02-14 09:54:37 +0300

    Verst

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,49 @@
 @extends('layouts.layouts')
 
 @section('content')
+<div class="hero bg-base-100">
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Copy-Star</h1>
+      <p class="py-6">Крупнейший магазин принтеров в России</p>
+      <button class="btn btn-primary">ф  Выбери свой</button>
+    </div>
+  </div>
+</div>
 
-Это главная страница
+<div class="text-center">
+<h1>Популярные Товары </h1>
+</div>
+
+<div class="carousel w-full">
+  <div id="slide1" class="carousel-item relative w-full">
+    <img src="/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" class="w-full" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide4" class="btn btn-circle">❮</a>
+      <a href="#slide2" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide2" class="carousel-item relative w-full">
+    <img src="/images/stock/photo-1609621838510-5ad474b7d25d.jpg" class="w-full" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide1" class="btn btn-circle">❮</a>
+      <a href="#slide3" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide3" class="carousel-item relative w-full">
+    <img src="/images/stock/photo-1414694762283-acccc27bca85.jpg" class="w-full" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide2" class="btn btn-circle">❮</a>
+      <a href="#slide4" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+  <div id="slide4" class="carousel-item relative w-full">
+    <img src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" class="w-full" />
+    <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+      <a href="#slide3" class="btn btn-circle">❮</a>
+      <a href="#slide1" class="btn btn-circle">❯</a>
+    </div>
+  </div>
+</div>
 
 @endsection
--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -18,53 +18,28 @@
   </div>
   <div class="flex-none">
     <ul class="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li tabindex="0">
-        <a>
-          Parent
-          <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
-        </a>
-        <ul class="p-2 bg-base-100">
-          <li><a>Submenu 1</a></li>
-          <li><a>Submenu 2</a></li>
-        </ul>
-      </li>
-      <li><a>Item 3</a></li>
-    </ul>
-  </div>
-</div>
-  @guest
+      <li><a href="{{ route('login') }}">О нас</a></li>
+      <li><a href="{{ route('login') }}">Где нас найти</a></li>
+      <li><a href="{{ route('login') }}">Каталог</a></li>
+      @guest
                             @if (Route::has('login'))
-
+                                    <li>
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Авторизация') }}</a>
-
+                                    </li>
                             @endif
 
                             @if (Route::has('register'))
-
+                                    <li>
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Регистрация') }}</a>
-
+                                    </li>
                             @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
 
-                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Выход') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
                         @endguest
-  <div id="indicator"></div>
+    </ul>
+  </div>
+</div>
+
 
 </nav>
 
@@ -72,5 +47,21 @@
             @yield('content')
         </main>
 
+        <footer class="footer footer-center p-10 bg-base-200 text-base-content rounded">
+  <div class="grid grid-flow-col gap-4">
+    <a class="link link-hover">О нас</a>
+    <a class="link link-hover">Где нас найти</a>
+    <a class="link link-hover">Каталог</a>
+    <a class="link link-hover">Профиль</a>
+  </div>
+  <div>
+    <div class="grid grid-flow-col gap-4">
+     <h1><a href="">VK</a></h1>
+    </div>
+  </div>
+  <div>
+    <p>Copyright © 2023 - Создатель <a href="">Dante-Alegori</a></p>
+  </div>
+</footer>
 </body>
 </html>
