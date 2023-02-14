d92ae29d1bb45b687bfddcd66a6f77b70121ecd0
Данил Алексеевич Зайцев <Giveknotgiveknot@gmail.com>
2023-02-14 12:34:23 +0300

    Registration-and-login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,8 +15,8 @@
 
             <form class="mt-10" method="POST">
                 <!-- Email Input -->
-                <label for="email" class="block text-xs font-semibold text-gray-600 uppercase">E-mail</label>
-                <input id="email" type="email" name="email" placeholder="e-mail address" autocomplete="email"
+                <label for="login" class="block text-xs font-semibold text-gray-600 uppercase">Логин</label>
+                <input id="login" type="string" name="login" placeholder="Логин" autocomplete="login"
                     class="block w-full py-3 px-1 mt-2
                     text-gray-800 appearance-none
                     border-b-2 border-gray-100
@@ -24,8 +24,8 @@
                     required />
 
                 <!-- Password Input -->
-                <label for="password" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Password</label>
-                <input id="password" type="password" name="password" placeholder="password" autocomplete="current-password"
+                <label for="password" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Пароль</label>
+                <input id="password" type="password" name="password" placeholder="Пароль" autocomplete="current-password"
                     class="block w-full py-3 px-1 mt-2 mb-4
                     text-gray-800 appearance-none
                     border-b-2 border-gray-100
@@ -37,21 +37,21 @@
                     class="w-full py-3 mt-10 bg-gray-800 rounded-sm
                     font-medium text-white uppercase
                     focus:outline-none hover:bg-gray-700 hover:shadow-none">
-                    Login
+                    Войти
                 </button>
 
                 <!-- Another Auth Routes -->
                 <div class="sm:flex sm:flex-wrap mt-8 sm:mb-4 text-sm text-center">
                     <a href="forgot-password" class="flex-2 underline">
-                        Forgot password?
+                        Забыли пароль?
                     </a>
 
                     <p class="flex-1 text-gray-500 text-md mx-4 my-1 sm:my-auto">
-                        or
+                        или
                     </p>
 
                     <a href="register" class="flex-2 underline">
-                        Create an Account
+                        Зарегистрироватся
                     </a>
                 </div>
             </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -14,9 +14,46 @@
             </h2>
 
             <form class="mt-10" method="POST">
+
+                <!-- Name Input -->
+                <label for="name" class="block text-xs font-semibold text-gray-600 uppercase">Имя</label>
+                <input id="name" type="string" name="name" placeholder="Ваше имя" autocomplete="Name"
+                    class="block w-full py-3 px-1 mt-2
+                    text-gray-800 appearance-none
+                    border-b-2 border-gray-100
+                    focus:text-gray-500 focus:outline-none focus:border-gray-200"
+                    required />
+
+                 <!--Surname Input -->
+                 <label for="surname" class="block text-xs font-semibold text-gray-600 uppercase">Фамилия</label>
+                <input id="surname" type="string" name="surname" placeholder="Ваша фамилия" autocomplete="Surname"
+                    class="block w-full py-3 px-1 mt-2
+                    text-gray-800 appearance-none
+                    border-b-2 border-gray-100
+                    focus:text-gray-500 focus:outline-none focus:border-gray-200"
+                    required />
+
+                 <!-- Patronymic Input -->
+                 <label for="patronymic" class="block text-xs font-semibold text-gray-600 uppercase">Отчество</label>
+                <input id="patronymic" type="string" name="patronymic" placeholder="Ваше Отчество" autocomplete="Patronymic"
+                    class="block w-full py-3 px-1 mt-2
+                    text-gray-800 appearance-none
+                    border-b-2 border-gray-100
+                    focus:text-gray-500 focus:outline-none focus:border-gray-200"
+                    required />
+
+                 <!-- Login Input -->
+                 <label for="login" class="block text-xs font-semibold text-gray-600 uppercase">Логин</label>
+                <input id="login" type="string" name="login" placeholder="Ваш логин" autocomplete="Login"
+                    class="block w-full py-3 px-1 mt-2
+                    text-gray-800 appearance-none
+                    border-b-2 border-gray-100
+                    focus:text-gray-500 focus:outline-none focus:border-gray-200"
+                    required />
+
                 <!-- Email Input -->
                 <label for="email" class="block text-xs font-semibold text-gray-600 uppercase">E-mail</label>
-                <input id="email" type="email" name="email" placeholder="e-mail address" autocomplete="email"
+                <input id="email" type="email" name="email" placeholder="e-mail" autocomplete="email"
                     class="block w-full py-3 px-1 mt-2
                     text-gray-800 appearance-none
                     border-b-2 border-gray-100
@@ -24,34 +61,51 @@
                     required />
 
                 <!-- Password Input -->
-                <label for="password" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Password</label>
-                <input id="password" type="password" name="password" placeholder="password" autocomplete="current-password"
+                <label for="password" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Пароль</label>
+                <input id="password" type="password" name="password" placeholder="Пароль" autocomplete="current-password"
                     class="block w-full py-3 px-1 mt-2 mb-4
                     text-gray-800 appearance-none
                     border-b-2 border-gray-100
                     focus:text-gray-500 focus:outline-none focus:border-gray-200"
                     required />
 
-                <!-- Auth Buttton -->
+
+                     <!-- Password repit -->
+                <label for="password" class="block mt-2 text-xs font-semibold text-gray-600 uppercase">Повторите пароль</label>
+                <input id="password" type="password" name="password" placeholder="Повторите пароль" autocomplete="current-password"
+                    class="block w-full py-3 px-1 mt-2 mb-4
+                    text-gray-800 appearance-none
+                    border-b-2 border-gray-100
+                    focus:text-gray-500 focus:outline-none focus:border-gray-200"
+                    required />
+
+                    <div class="form-control">
+                        <label class="cursor-pointer label">
+                            <span class="label-text">Соглашаюсь с правилами</span>
+                                 <input type="checkbox" checked="checked" class="checkbox checkbox-accent" />
+                     </label>
+                    </div>
+
+                <!-- Registration Buttton -->
                 <button type="submit"
                     class="w-full py-3 mt-10 bg-gray-800 rounded-sm
                     font-medium text-white uppercase
                     focus:outline-none hover:bg-gray-700 hover:shadow-none">
-                    Login
+                    Registration
                 </button>
 
                 <!-- Another Auth Routes -->
                 <div class="sm:flex sm:flex-wrap mt-8 sm:mb-4 text-sm text-center">
                     <a href="forgot-password" class="flex-2 underline">
-                        Forgot password?
+                        Забыли пароль?
                     </a>
 
                     <p class="flex-1 text-gray-500 text-md mx-4 my-1 sm:my-auto">
-                        or
+                        или
                     </p>
 
                     <a href="register" class="flex-2 underline">
-                        Create an Account
+                        Войти
                     </a>
                 </div>
             </form>
